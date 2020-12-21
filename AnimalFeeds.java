b4785d594666830e1b7bd9bd9d7cc12ea112f98e
MAXON MACHEDA MR 200 <bsc-com-ne-12-18@cc.ac.mw>
2020-12-21 21:34:25 +0000

    comments were added in abstract classs

--- AnimalFeeds.java
+++ AnimalFeeds.java
@@ -1,4 +1,8 @@
 public abstract class  AnimalFeeds{
+	//Abstract methods to be implemented in AnimalFeed class that extends this class.
+
+	//A method to get the total kgs to be made
     public abstract double getSum();
+    //a methodd to get the whole details fof the feed to be made.
     public  abstract  String getAnimalFeedResults();
 }
